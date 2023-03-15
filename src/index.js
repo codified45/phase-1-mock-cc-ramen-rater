9d429a661f7d4f1ad91a44090c686a44ea570843
Jack Lach <jack.lach4@gmail.com>
2023-03-15 18:28:22 -0400

    complete functionality of displayRamenDetail()

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const currentRamensUrl = "http://localhost:3000/ramens";
 
 document.addEventListener("DOMContentLoaded", function() {
     const ramenMenu = document.getElementById('ramen-menu');
+    const ramenDetails = document.getElementById('ramen-detail');
 
     displayRamensInMenu();
 
@@ -13,14 +14,30 @@ document.addEventListener("DOMContentLoaded", function() {
             for (const element of obj) {
                 const img = document.createElement('img');
                 img.src = element.image;
+                img.id = element.id;
                 img.addEventListener('click', displayRamenDetail);
                 ramenMenu.append(img);
+
             };
         });
     };
 
     function displayRamenDetail(e) {
-
+        const chosenRamenUrl = currentRamensUrl + `/${e.target.id}`;
+        const name = document.querySelector('h2.name');
+        const restaurant = document.querySelector('h3.restaurant');
+        const comment = document.getElementById('comment-display');
+        const rating = document.getElementById('rating-display');
+        const image = document.querySelector('img.detail-image');
+        fetch(chosenRamenUrl)
+        .then(res => res.json())
+        .then(obj => {
+        image.src = obj.image;
+        name.textContent = obj.name;
+        restaurant.textContent = obj.restaurant;
+        rating.textContent = obj.rating;
+        comment.textContent = obj.comment;
+        });
     };
 
 });
