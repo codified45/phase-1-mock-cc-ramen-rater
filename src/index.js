32f0d5c1d60dc13dc8604a57b645ac026ba5e682
Jack Lach <jack.lach4@gmail.com>
2023-03-15 17:57:40 -0400

    complete initial functionality of  displayRamensInMenu()

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ const currentRamensUrl = "http://localhost:3000/ramens";
 
 document.addEventListener("DOMContentLoaded", function() {
     const ramenMenu = document.getElementById('ramen-menu');
-    
+
+    displayRamensInMenu();
+
+    function displayRamensInMenu() {
+        fetch(currentRamensUrl)
+        .then(res => res.json())
+        .then(obj => {
+            for (const element of obj) {
+                const img = document.createElement('img');
+                img.src = element.image;
+                img.addEventListener('click', displayRamenDetail);
+                ramenMenu.append(img);
+            };
+        });
+    };
+
+    function displayRamenDetail(e) {
+
+    };
 
 });
