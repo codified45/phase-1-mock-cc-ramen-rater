119fff3e1d163b41d8e560a305b479f44ba2557c
Jack Lach <jack.lach4@gmail.com>
2023-03-15 08:16:17 -0400

    add event listener for DOMContentLoaded and API Url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// write your code here
+const currentRamensUrl = "http://localhost:3000/ramens";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ramenMenu = document.getElementById('ramen-menu');
+    
+
+});
